4d61db380b46da5afac377762131a5467a3f1df6
harol182014@gmail.com <harol182014@gmail.com>
2024-03-20 09:27:21 -0500

    new

---
+++ backend/src/components/user/components/user/UserDahboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserDashboard = () => {
+  return (
+    <div>
+      <h1>User Dashboard</h1>
+      {/* Contenido del dashboard del usuario */}
+    </div>
+  );
+};
+
+export default UserDashboard;
---
+++ backend/src/components/user/components/user/Userprofile.js
@@ -0,0 +1,13 @@
+// UserProfile.js
+import React from 'react';
+
+const UserProfile = () => {
+  return (
+    <div>
+      <h1>User Profile</h1>
+      {/* Contenido del perfil del usuario */}
+    </div>
+  );
+};
+
+export default UserProfile;
